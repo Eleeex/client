ae768bee86b25c2f606c28c90fde18b46796a1f9
JaysonFidiles <fidilesjayson@gmail.com>
2024-09-28 11:03:56 +0800

    tulog sako kapoy na

---
+++ task1.py
@@ -0,0 +1,13 @@
+age = [-12,13,-14,15,-16]
+username==["juan", "pedro", "james", "bong cute", "natoy"]
+
+for dataAge in age:
+    if dataAge < 0:
+        print(str(dataAge) + "negative")
+        elif dataAge . 0:
+            print(str(dataAge) + "positive")
+
+for dataUser in username:
+    if datauser =="bong cute"
+else:
+    print("bong cute wala")
