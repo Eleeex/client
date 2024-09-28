3072fd3725d2bd73a3c2aaa5c5a75a2bee340f94
JaysonFidiles <torinoclientruin@gmail.com>
2024-09-28 11:15:43 +0800

    task1 loop

--- task1.py
+++ task1.py
@@ -1,13 +1,13 @@
-age = [-12,13,-14,15,-16]
+age = [-12, 13, -14, 15, -16 ]
 username==["juan", "pedro", "james", "bong cute", "natoy"]
 
 for dataAge in age:
     if dataAge < 0:
         print(str(dataAge) + "negative")
-        elif dataAge . 0:
-            print(str(dataAge) + "positive")
-
+        elif dataAge > 0:
+            print(str(dareAge) + "positive"
+            
 for dataUser in username:
-    if datauser =="bong cute"
+    If dateUser =="bong cute"
 else:
     print("bong cute wala")
